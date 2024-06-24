d993e30dbcaa194cb6cf0c7e2638da904b12ce9a
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2024-06-24 23:26:05 +0700

    Refactor function names and remove SSE 4.2 support

--- include/rapidutf/rapidutf.hpp
+++ include/rapidutf/rapidutf.hpp
@@ -37,12 +37,12 @@ public:
   static auto wide_to_utf8(const std::wstring &wide) -> std::string;
 
 private:
-  static auto utf8_to_utf16_common(const unsigned char *bytes, std::size_t length, std::u16string &utf16) -> void;
-  static auto utf16_to_utf8_common(const char16_t *chars, std::size_t length, std::string &utf8) -> void;
-  static auto utf16_to_utf32_common(const char16_t *chars, std::size_t length, std::u32string &utf32) -> void;
-  static auto utf32_to_utf16_common(const char32_t *chars, std::size_t length, std::u16string &utf16) -> void;
-  static auto utf8_to_utf32_common(const unsigned char *bytes, std::size_t length, std::u32string &utf32) -> void;
-  static auto utf32_to_utf8_common(const char32_t *chars, std::size_t length, std::string &utf8) -> void;
+  static auto utf8_to_utf16_scalar(const unsigned char *bytes, std::size_t length, std::u16string &utf16) -> void;
+  static auto utf16_to_utf8_scalar(const char16_t *chars, std::size_t length, std::string &utf8) -> void;
+  static auto utf16_to_utf32_scalar(const char16_t *chars, std::size_t length, std::u32string &utf32) -> void;
+  static auto utf32_to_utf16_scalar(const char32_t *chars, std::size_t length, std::u16string &utf16) -> void;
+  static auto utf8_to_utf32_scalar(const unsigned char *bytes, std::size_t length, std::u32string &utf32) -> void;
+  static auto utf32_to_utf8_scalar(const char32_t *chars, std::size_t length, std::string &utf8) -> void;
 
 #if defined(RAPIDUTF_USE_AVX2)
   static auto utf8_to_utf16_avx2(const std::string &utf8) -> std::u16string;
@@ -51,13 +51,6 @@ private:
   static auto utf32_to_utf16_avx2(const std::u32string &utf32) -> std::u16string;
   static auto utf8_to_utf32_avx2(const std::string &utf8) -> std::u32string;
   static auto utf32_to_utf8_avx2(const std::u32string &utf32) -> std::string;
-#elif defined(RAPIDUTF_USE_SSE_4_2)
-  static auto utf8_to_utf16_sse42(const std::string &utf8) -> std::u16string;
-  static auto utf16_to_utf8_sse42(const std::u16string &utf16) -> std::string;
-  static auto utf16_to_utf32_sse42(const std::u16string &utf16) -> std::u32string;
-  static auto utf32_to_utf16_sse42(const std::u32string &utf32) -> std::u16string;
-  static auto utf8_to_utf32_sse42(const std::string &utf8) -> std::u32string;
-  static auto utf32_to_utf8_sse42(const std::u32string &utf32) -> std::string;
 #elif defined(RAPIDUTF_USE_NEON)
   static auto utf8_to_utf16_neon(const std::string &utf8) -> std::u16string;
   static auto utf16_to_utf8_neon(const std::u16string &utf16) -> std::string;
